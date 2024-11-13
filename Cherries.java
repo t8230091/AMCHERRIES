d8fc29661c13f92210b7efe7e0c04ce4aed9982a
Eleftheria Matsouka <t8230091@aueb.gr>
2024-11-13 23:46:22 +0200

    Added the main method

--- Cherries.java
+++ Cherries.java
@@ -1,10 +1,11 @@
 public class Cherries {
-    System.out.println("8230091");
-    System.out.println("8230109");
-    System.out.println("8230043");
-    System.out.println("8230070");
-    System.out.println("8230164");
-    System.out.println("8230165");
-    System.out.println("8230150");
-    
+    public static void main(String[] args) {
+        System.out.println("8230091");
+        System.out.println("8230109");
+        System.out.println("8230043");
+        System.out.println("8230070");
+        System.out.println("8230164");
+        System.out.println("8230165");
+        System.out.println("8230150");
+    }
 }
